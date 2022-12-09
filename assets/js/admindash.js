12ee82e61b1307647d0ce8ea03d5e61c7b1f958f
benod <benoitnda35@gmail.com>
2022-12-09 15:14:41 +0200

    working on dashbords and collecting buttons in mobile devices

--- assets/js/admindash.js
+++ assets/js/admindash.js
@@ -1,24 +1,74 @@
-let createBtn=document.querySelector("#create");
-let mngBlogBtn=document.querySelector("#mnge-blog");
-let UpdateBtn=document.querySelector("#update");
-let editForm=document.querySelector(".edit-form");
-let createBlogForm=document.querySelector(".create-blog");
-let ManageBlogTable=document.querySelector(".blog-table");
+//blog variables
+let createBtn = document.querySelector("#create");
+let mngBlogBtn = document.querySelector("#mnge-blog");
+let UpdateBtn = document.querySelector("#update");
+let editForm = document.querySelector(".edit-form");
+let createBlogForm = document.querySelector(".create-blog");
+let ManageBlogTable = document.querySelector(".blog-table");
+//portifolio section variable
+let addpojectBtn = document.querySelector("#new-project");
+let mngProjectBtn = document.querySelector("#projects");
+let createProjectForm = document.querySelector(".create-project");
+let ManageProjectTable = document.querySelector(".project-table");
+let UpdateprojectBtn = document.querySelector("#update-project");
+//swiching menus varibales
+let portifolioBtn = document.querySelector("#portifolio-menu");
+let blogBtn = document.querySelector("#blog-menu");
+let SkillBtn = document.querySelector("#skills-menu");
+let portifolioSCTN = document.querySelector(".portifolio-section");
+let blogSCTN = document.querySelector(".blogs-section");
+let skillSCTN = document.querySelector(".skills-section");
+
+blogBtn.addEventListener("click", () => {
+  portifolioSCTN.classList.remove("show-section");
+  blogSCTN.classList.add("show-section");
+  blogSCTN.classList.remove("hide-section");
+  portifolioSCTN.classList.add("hide-section");
+  skillSCTN.classList.add("hide-section");
+  });
+  portifolioBtn.addEventListener("click", () => {
+   portifolioSCTN.classList.remove("hide-section");
+   portifolioSCTN.classList.add("show-section");
+   skillSCTN.classList.add("hide-section");
+    });
+    SkillBtn.addEventListener("click", () => {
+      skillSCTN.classList.remove("hide-section");
+      skillSCTN.classList.add("show-section");
+       });
+  console.log(blogBtn);
 //manage blog button
- mngBlogBtn.addEventListener('click',()=>{
-    createBlogForm.classList.add('hide-blog-form');
-    ManageBlogTable.classList.add('blog-table-show');
-    ManageBlogTable.classList.remove('hide-blogs');
- });
- //create blog button
- createBtn.addEventListener('click',()=>{
-    ManageBlogTable.classList.remove('blog-table-show');
-    ManageBlogTable.classList.add('hide-blogs');
-    createBlogForm.classList.add('show-blog');
-     createBlogForm.classList.remove('hide-blog-form');
+mngBlogBtn.addEventListener("click", () => {
+  createBlogForm.classList.add("hide-blog-form");
+  ManageBlogTable.classList.add("blog-table-show");
+  ManageBlogTable.classList.remove("hide-blogs");
 });
- // update button
-UpdateBtn.addEventListener('click',()=>{
- editForm.classList.add('show-blog');
- editForm.classList.remove('hide-blogs');
+//create blog button
+createBtn.addEventListener("click", () => {
+  ManageBlogTable.classList.remove("blog-table-show");
+  ManageBlogTable.classList.add("hide-blogs");
+  createBlogForm.classList.add("show-blog");
+  createBlogForm.classList.remove("hide-blog-form");
+});
+// update button
+UpdateBtn.addEventListener("click", () => {
+  editForm.classList.add("show-blog");
+  editForm.classList.remove("hide-blogs");
+});
+//add project button
+addpojectBtn.addEventListener("click", () => {
+  ManageProjectTable.classList.remove("project-table-show");
+  ManageProjectTable.classList.add("hide-project");
+  createProjectForm.classList.add("show-project");
+  createProjectForm.classList.remove("hide-project-form");
+});
+//manage project button
+mngProjectBtn.addEventListener("click", () => {
+  createProjectForm.classList.add("hide-project-form");
+  ManageProjectTable.classList.add("project-table-show");
+  ManageProjectTable.classList.remove("hide-project");
+});
+// update project button
+UpdateprojectBtn.addEventListener("click", () => {
+  editForm.classList.add("show-project");
+  editForm.classList.remove("hide-project");
 });
