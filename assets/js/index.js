d6e5fa9866a06af9c30d006702cf4e32756cdacf
benod <benoitnda35@gmail.com>
2022-12-11 09:14:37 +0200

    adding responsive nav bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 let logo=document.querySelector("#logo");
 let home=document.querySelector("#home");
 let lognForm=document.querySelector("#loginPage");
+let menuIcon=document.querySelector(".menu-icon");
+let closeIcon=document.querySelector(".close");
+let mobileMenu=document.querySelector(".mobile-menus");
 logo.addEventListener('click',function(){
     lognForm.classList.add('showLogn');
     lognForm.classList.remove('hideLogn');
@@ -9,3 +12,15 @@ home.addEventListener('click',function(){
     lognForm.classList.remove('showLogn');
     lognForm.classList.add('hideLogn');
 });
+menuIcon.addEventListener('click',()=>{
+    closeIcon.classList.add('make-visible');
+    mobileMenu.classList.add('make-visible');
+    menuIcon.classList.remove('make-visible');
+    mobileMenu.classList.remove('make-invisible');
+    });
+closeIcon.addEventListener('click',()=>{
+    mobileMenu.classList.remove('make-visible');
+    mobileMenu.classList.add('make-invisible');
+    closeIcon.classList.remove('make-visible');
+    menuIcon.classList.add('make-visible');
+   });
