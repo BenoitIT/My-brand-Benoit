5106c258de378feea624e1ffdf8cd468e6da47ea
benoitIT <benoitnda35@gmail.com>
2023-02-06 10:39:23 +0200

    handling image uplaod in form data

--- assets/js/ApisData/dashboard/postBlog.js
+++ assets/js/ApisData/dashboard/postBlog.js
@@ -1,48 +1,42 @@
-let reader = new FileReader();
-let imgView = document.querySelector(".imge-preview");
-let blogImage = document.querySelector("#image-upload");
-let blogTopic = document.querySelector("#topic");
-let blogTitle = document.querySelector("#Title");
-let quillEditor = document.querySelector("#blog-editor");
-let image;
-let fileItem;
-let fileName;
-let uploadedImageURL;
-//function to becalled
-function getFile(e) {
-  fileItem = e.target.files[0];
-  fileName = fileItem.name;
-}
+const imgView = document.querySelector(".imge-preview");
+const blogImage = document.querySelector("#image-upload");
+const blogTopic = document.querySelector("#topic");
+const blogTitle = document.querySelector("#Title");
+const quillEditor = document.querySelector("#blog-editor");
+const formData = new FormData();
 blogImage.addEventListener("change", (event) => {
   event.preventDefault();
-  imgView.classList.add("show-preview");
-  imgView.classList.remove("imge-preview");
-  getFile(event);
-  reader.addEventListener("load", () => {
-    image = reader.result;
-    imgView.src = image;
-  });
-  reader.readAsDataURL(fileItem);
+  const file = blogImage.files[0];
+
+  if (file) {
+    formData.append("blogImage", file);
+    imgView.classList.add("show-preview");
+    imgView.classList.remove("imge-preview");
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+      imgView.src = reader.result;
+    });
+    reader.readAsDataURL(file);
+  }
 });
 // ===== Adding blog ====
 const UploadBlogForm = document.querySelector("#blog-form");
 UploadBlogForm &&
   UploadBlogForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    fetch("https://dead-jade-coypu-cape.cyclic.app/Api/blogs/new",
-      {
-      method:'POST',
+    formData.append("title", blogTitle.value);
+    formData.append("category", blogTopic.value);
+    formData.append("blogDescription", quillEditor.textContent);
+
+    fetch("https://dead-jade-coypu-cape.cyclic.app/Api/blogs/new", {
+      method: "POST",
       headers: {
-          "Authorization": `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({
-        title: blogTopic.value,
-        category: blogTitle.value,
-        blogDescription: quillEditor.textContent,
-        blogImage:image,
-      }),
-    }).then(res=>res.json())
-     .then(data=>{
-      alert(data.message)
-     });
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message)
+      });
   });
